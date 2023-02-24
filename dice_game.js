66ae3f149bb9988c330f76b8128502f1c74d329b
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-02-24 22:44:50 +0200

    js:created random image generator

--- dice_game.js
+++ dice_game.js
@@ -1,3 +1,19 @@
 window.onload=function(){
+    function randoms(){
+        var random_images= new Array();
+        random_images[0]="Assets\dice-1.png"
+        random_images[1]="Assets\dice-2.png"
+        random_images[2]="Assets\dice-3.png"
+        random_images[3]="Assets\dice-4.png"
+        random_images[4]="Assets\dice-5.png"
+        random_images[5]="Assets\dice-6.png"
+        var random_1 = Math.floor(Math.random()*random_images.length);  
+        var random2 = Math.floor(Math.random()*random_images.length);  
+        
+
+
+
+    }
+
 
 }
